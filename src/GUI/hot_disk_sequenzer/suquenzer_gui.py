d4709ac4878412a1a870bd73ec5a205deb04f3f4
Kiki <Christian.Wagner.420@gmail.com>
2024-11-25 15:18:17 +0100

    moved sequenzer to own folder. temporarily. just for sending to hot disk because i have questioins

--- src/GUI/hot_disk_sequenzer/suquenzer_gui.py
+++ src/GUI/hot_disk_sequenzer/suquenzer_gui.py
@@ -1,9 +1,8 @@
 import datetime
-
 from PySide6.QtWidgets import (
     QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel,
     QLineEdit, QMessageBox, QComboBox, QFileDialog, QTableWidget, QTableWidgetItem,
-    QHeaderView, QSizePolicy, QFormLayout
+    QHeaderView, QSizePolicy, QFormLayout, QGroupBox
 )
 from PySide6.QtGui import QIntValidator
 
@@ -23,8 +22,8 @@ class ScheduleGeneratorBase(QWidget):
 
         # Add more input fields as needed
         self._init_buttons()
+        self._init_measurement_settings()  # Initialize measurement settings
         self._init_schedule_table()
-
         self._init_repetition_buttons()
         self.setLayout(self.layout)
 
@@ -40,6 +39,55 @@ class ScheduleGeneratorBase(QWidget):
         self.plot_program_button = QPushButton('Plot Schedule')
         self.plot_program_button.clicked.connect(self.plot_program)
         self.layout.addWidget(self.plot_program_button)
+
+    def _init_measurement_settings(self):
+        # Measurement settings group
+        measurement_group = QGroupBox("Measurement Settings")
+        measurement_layout = QFormLayout()
+
+        # start time program
+        self.start_measurements_label = QLabel("Start time temperature program:")
+        self.start_measurements_input = QLineEdit(str(datetime.datetime.now()))
+        self.start_measurements_input.setFixedWidth(100)
+        measurement_layout.addRow(self.start_measurements_label, self.start_measurements_input)
+
+
+        # Number of Measurements
+        self.num_measurements_label = QLabel("Number of Measurements:")
+        self.num_measurements_input = QLineEdit("3")
+        self.num_measurements_input.setValidator(QIntValidator(1, 1000))
+        self.num_measurements_input.setFixedWidth(50)
+        measurement_layout.addRow(self.num_measurements_label, self.num_measurements_input)
+
+        # Measurement Interval
+        self.measurement_interval_label = QLabel("Measurement Interval (min):")
+        self.measurement_interval_input = QLineEdit("3")
+        self.measurement_interval_input.setValidator(QIntValidator(1, 1000))
+        self.measurement_interval_input.setFixedWidth(50)
+        measurement_layout.addRow(self.measurement_interval_label, self.measurement_interval_input)
+
+        # Template File
+        self.template_file_label = QLabel("Template File:")
+        self.template_file_path = QLineEdit()
+        self.template_file_path.setText(r"C:\Daten\Kiki\ProgrammingStuff\t_p_etc_recorder_v2\src\GUI\hot_disk_sequenzer\template.hseq")
+        self.template_file_browse_button = QPushButton("Browse")
+        self.template_file_browse_button.clicked.connect(self.browse_template_file)
+        template_file_layout = QHBoxLayout()
+        template_file_layout.addWidget(self.template_file_path)
+        template_file_layout.addWidget(self.template_file_browse_button)
+        measurement_layout.addRow(self.template_file_label, template_file_layout)
+
+        measurement_group.setLayout(measurement_layout)
+
+        self.layout.addWidget(measurement_group)
+
+    def browse_template_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file_name, _ = QFileDialog.getOpenFileName(self, "Select Template File", "",
+                                                   "XML Files (*.xml);;All Files (*)", options=options)
+        if file_name:
+            self.template_file_path.setText(file_name)
 
     def _init_schedule_table(self):
         # Measurement table
@@ -122,29 +170,33 @@ class ScheduleGeneratorMain(ScheduleGeneratorBase):
 
     def generate_schedule(self):
         # Collect all measurements from the table
-        # ...
         scheduled_program = self.parse_program()
-        scheduled_program_unique_temperatures = scheduled_program.drop_duplicates(subset=['temperature'], keep='first')
-        temperatures = [scheduled_program_unique_temperatures['temperature']]
+        scheduled_program_unique_temperatures = scheduled_program.drop_duplicates(subset=['temperature'], keep='last')
+        temperatures = scheduled_program_unique_temperatures['temperature'].tolist()
         heating_powers = {}
         meas_times = {}
-        NPLC = {}
 
         for index, row in scheduled_program_unique_temperatures.iterrows():
             heating_powers[row['temperature']] = row['measurement_power_watt']
             meas_times[row['temperature']] = row['measurement_time']
-            NPLC[row['temperature']] = 0.5
 
-        schedule_creator = ScheduleCreator(sample_temperatures=temperatures,
-                                           heating_powers=heating_powers,
-                                           heating_time=meas_times)
-
-        schedule_creator.create_schedule(schedule_df=scheduled_program,
-                                         output_file="test.hseq")
+        # Get the number of measurements, measurement interval, and template file
+        num_measurements = int(self.num_measurements_input.text())
+        measurement_interval = int(self.measurement_interval_input.text())
+        template_file = self.template_file_path.text()
 
 
+        # Initialize ScheduleCreator with the retrieved values
+        schedule_creator = ScheduleCreator(
+            sample_temperatures=temperatures,
+            heating_powers=heating_powers,
+            heating_times=meas_times,
+            no_of_measurements=num_measurements,
+            measurement_interval=measurement_interval,
+            template_file=template_file
+        )
 
-
+        schedule_creator.create_schedule(schedule_df=scheduled_program, output_file="test.hseq")
 
         print(heating_powers)
         print(scheduled_program)
@@ -188,15 +240,16 @@ class ScheduleGeneratorMain(ScheduleGeneratorBase):
     def parse_program(self):
         program = self.get_temperature_program()
         repeat_start, repeat_end, repeat_count = self.get_repetition_parameters()
-        temperature_controller = TpProgramSimulator().TemperatureController(temperature_program=program,
-                                                                            repeat_start=repeat_start,
-                                                                            repeat_end=repeat_end,
-                                                                            repeat_count=repeat_count)
-        start_time = datetime.datetime.now() #self.program_start_time
+        temperature_controller = TpProgramSimulator().TemperatureController(
+            temperature_program=program,
+            repeat_start=repeat_start,
+            repeat_end=repeat_end,
+            repeat_count=repeat_count
+        )
+        start_time = datetime.datetime.strptime(self.start_measurements_input.text(), "%Y-%m-%d %H:%M:%S.%f")  # self.program_start_time
 
         program_with_meas_times = temperature_controller.get_program_times(start_time=start_time)
         return program_with_meas_times
-
 
 
 if __name__ == '__main__':
