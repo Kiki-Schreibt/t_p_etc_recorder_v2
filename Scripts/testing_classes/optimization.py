31943440d6e99b4f6eb314ef06629928d139f6ef
Kiki <Christian.Wagner.420@gmail.com>
2024-11-27 15:28:25 +0100

    cleaned up imports and a billion lines of unused trash... one billion more to go

--- Scripts/testing_classes/optimization.py
+++ Scripts/testing_classes/optimization.py
@@ -1,44 +1,16 @@
-import threading
-from datetime import datetime, timedelta
-import pandas as pd
-import numpy as np
-import win32com.client as win32
-import re
-import os
-import struct
 import time
 from zoneinfo import ZoneInfo
 
-from src.config_connection_reading_management.connections import DatabaseConnection, GetConfig, ModbusConnection
+from src.config_connection_reading_management.connections import DatabaseConnection
 from src.config_connection_reading_management.logger import AppLogger
-from pymodbus.exceptions import ModbusException, ConnectionException
-from pymodbus.pdu import ExceptionResponse
-from src.calculations.eq_p_calculation import VantHoffCalcEq as EqCalculator
+
 from src.config_connection_reading_management.query_builder import QueryBuilder
-from src.meta_data.meta_data_handler import MetaData
 from src.table_data import TableConfig
 
 qb = QueryBuilder()
-# Load the configuration
-config = GetConfig()
-# Accessing the variables
-LOG_DIRECTORY   = config.LOG_DIRECTORY
-LOG_FILE        = config.LOG_FILE
-DB_SERVER       = config.DB_SERVER
-DB_DATABASE     = config.DB_DATABASE
-DB_USERNAME     = config.DB_USERNAME
-DB_PASSWORD     = config.DB_PASSWORD
-DB_PORT         = config.DB_PORT
-MODBUS_HOST     = config.MODBUS_HOST
-MODBUS_PORT     = config.MODBUS_PORT
-REGS_OF_INTEREST = config.REGS_OF_INTEREST
-START_REG       = config.START_REG
-END_REG         = config.END_REG
-SLEEP_INTERVAL  = config.SLEEP_INTERVAL
 
 local_tz = ZoneInfo("Europe/Berlin")
 table_rp = TableConfig().TPDataTable
-
 
 
 class Problem:
@@ -46,7 +18,6 @@ class Problem:
     def __init__(self):
         self.running = False
         self.qb = qb
-        self.config = config
         self.logger = AppLogger().get_logger(__name__)
         self.database_connection = DatabaseConnection()
        # self.database_connection.open_connection()  # Open the connection
